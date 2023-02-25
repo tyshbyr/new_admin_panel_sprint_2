840a909622cc4ad022f0db9ddc88bd8a4256ea54
tyshbyr <tyshbyr@gmail.com>
2023-02-26 03:36:18 +0500

    Добавлен скрипт ожидания готовности БД

--- docker_compose/simple_project/app/run_uwsgi.sh
+++ docker_compose/simple_project/app/run_uwsgi.sh
@@ -4,4 +4,11 @@ set -e
 
 chown www-data:www-data /var/log
 
-uwsgi --strict --ini /etc/app/uwsgi.ini
+while ! nc -z $DB_HOST $DB_PORT; do
+      sleep 0.1
+done
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+uwsgi --strict --ini /app/uwsgi.ini
